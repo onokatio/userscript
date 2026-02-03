49070a3dab7208ca4b78e1d028383c320715f93f
onokatio (tmaruyama) <contact+github.com@katio.net>
2026-02-03 18:34:01 +0900

    Update YoutubeAV1.user.js

--- YoutubeAV1.user.js
+++ YoutubeAV1.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/onokatio/userscript/
 // @match       *://www.youtube.com/*
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      onokatio
 // @description 2023/1/23 13:35:22
 // @updateURL    https://github.com/onokatio/userscript/raw/master/YoutubeAV1.user.js
@@ -12,4 +12,16 @@
 // ==/UserScript==
 
 const quality = '8192';
+const drc = {
+  "data": "0",
+  "expiration": 9999999999999,
+  "creation": Date.now()-1000,
+};
+const boost = {
+  "data": "0",
+  "expiration": 9999999999999,
+  "creation": Date.now()-1000,
+};
 localStorage.setItem('yt-player-av1-pref', quality);
+localStorage.setItem('yt-player-drc-pref', JSON.stringify(drc));
+localStorage.setItem('yt-player-voice-boost', JSON.stringify(boost));
